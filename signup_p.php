789e247b879627d8f493e4075fb9dbe7fcbe6e15
Shivani Karkera <karkerashivanigirish@gmail.com>
2021-12-26 09:34:54 +0530

    signup

---
+++ signup_p.php
@@ -0,0 +1,35 @@
+<?php
+
+    $username = $_POST['username'];
+    $password = $_POST['password1'];
+
+    $con = mysqli_connect("localhost", "root", "", "roombooking");
+
+    if (!$con) {
+       die( " Connection failed. Please try again: Error: " . mysqli_connect_error() );
+    }
+
+    //to check if the username already exists in the database
+
+    $username_existance_query = "SELECT usn from login where usn = '".$username."';";
+    $query = mysqli_query($con, $username_existance_query);
+
+    if(mysqli_num_rows($query) != 0)
+    {
+        die ("Username already exists");
+        
+    }
+
+    $query_stmt = "INSERT INTO login values('$username', '$password')";   
+
+    $query = mysqli_query($con, $query_stmt);
+
+    if (!$query) {
+        echo "Couldnt sign-up. please try again: Error: " . mysqli_error($con);
+    }
+
+header('Location: login.php');
+
+    mysqli_close($con);
+
+?>
